12614705a4aa8b3d7564716c42ff491a5c16925e
spectradedevv <psupgb3@mail.com>
2025-05-15 18:43:07 +0100

    lms-frontend

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import InstructorProvider from "./context/instructor-context/index.jsx";
 import StudentProvider from "./context/student-context/index.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/"> 
     <AuthProvider>
       <InstructorProvider>
         <StudentProvider>
